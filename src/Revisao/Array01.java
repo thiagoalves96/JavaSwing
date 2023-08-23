c2574ecf5814318a6a7f35d333b7a8d268cbc391
thiagoalves96 <thiagodev22@hotmail.com>
2023-08-23 19:21:54 -0300

    teste

---
+++ src/Revisao/Array01.java
@@ -0,0 +1,19 @@
+package Revisao;
+
+public class Array01 {
+
+	public static void main(String[] args) {
+		int a[] = new int[5];
+
+		a[0] = 10;
+		a[1] = 9;
+		a[2] = 7;
+		a[3] = 4;
+		a[4] = 7;
+
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i] + " ");
+
+	}
+
+}
